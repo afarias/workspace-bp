4d276448c68c625e16af03877fc4060562035d87
Leglaireux <jean.deglaire@gmail.com>
2016-03-28 20:45:29 -0300

    I did  the bid

--- src/jean/TenFirstPrimes.java
+++ src/jean/TenFirstPrimes.java
@@ -1,10 +1,29 @@
 package jean;
 
+import controlStructures.Math;
+
 /**
  * Created by andres on 3/28/16.
  */
 public class TenFirstPrimes {
+
+    /**
+     * This method is responsible for printing the ten first prime numbers.
+     * @param args It receives no arguments at all.
+     */
     public static void main(String[] args) {
+        Math math = new Math();
+
+        int nPrimes = 0;
+        int aNumber = 2;
+        while(nPrimes < 10){
+            if (math.isPrime(aNumber)){
+                System.out.println();
+                nPrimes++;
+            }
+
+            aNumber++;
+        }
 
     }
 }
