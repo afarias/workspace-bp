62ab946183d6055aa09d27282d4638c9cd7163ca
Leglaireux <jean.deglaire@gmail.com>
2016-04-05 20:07:29 -0300

    I did  the bid

--- src/jean/Interface.java
+++ src/jean/Interface.java
@@ -1,9 +1,20 @@
 package jean;
 
+import java.util.Scanner;
+
 /**
  * Created by Admin on 05/04/2016.
  */
 public class Interface {
+
+    public static void main(String[] args) {
+        String question1 = "Y/N ?";
+        String answers[] = {"Y", "N"};
+        Interface anInterface = new Interface();
+
+        String userAnswer = anInterface.askQuestion(question1,answers);
+        System.out.println(userAnswer);
+    }
 
     /**
      * This method is responsible for getting a valid response after asking a question.
@@ -12,8 +23,22 @@ public class Interface {
      * @param answer The set of valid answers
      * @return The valid answer choose by the user
      */
-    public String askQuestion(String question, String answer[]){
-        return null;
+    public String askQuestion(String question, String answer[]) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        boolean isValid = false;
+        String userInput;
+        do { // The program will test all possible answers until he find one valid.
+            System.out.println(question);
+            userInput = sc.nextLine();
+            for (i = 0; i <= answer.length; i++)
+                if (userInput.equals(answer[i])){
+                    isValid = true;
+                    break;
+                }
+        } while (!isValid);
+
+        return userInput;
     }
 
     /**
