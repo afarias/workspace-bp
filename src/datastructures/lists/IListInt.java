c4a2f98725030e751b545430d85ff348246d5e28
Andres Farias <andres.farias@blueprintsit.cl>
2016-05-02 18:19:09 -0300

    Interfaces and concrete classes for Jean.

---
+++ src/datastructures/lists/IListInt.java
@@ -0,0 +1,80 @@
+package datastructures.lists;
+
+/**
+ * Created by andres on 5/2/16.
+ */
+public interface IListInt {
+
+    /**
+     * This method is responsible for determining whether the list is empty or not.
+     *
+     * @return Returns <code>true</code> if the list is empty and <code>false</code> otherwise.
+     */
+    public boolean isEmpty();
+
+    /**
+     * This method is responsible for determining whether the list is full or not.
+     *
+     * @return Returns <code>true</code> if the list is full and <code>false</code> otherwise.
+     */
+    public boolean isFull();
+
+    /**
+     * This method is responsible for determining the current number of elements within the list.
+     *
+     * @return The number of numbers stored in the list.
+     */
+    public int size();
+
+    /**
+     * This method is responsible for determining the current capacity of the list.
+     * @return The current list capacity
+     */
+    public int capacity();
+
+    /**
+     * This method is responsible for storing the <code>number</code> at the beginning of the list.
+     *
+     * @param number The number to be inserted.
+     */
+    public void addAtBeginning(int number);
+
+    /**
+     * This method is responsible for storing the <code>number</code> at the end of the list.
+     *
+     * @param number The number to be inserted at the end of the list.
+     */
+    public void addAtEnd(int number);
+
+    /**
+     * This method is responsible for storing the <code>number</code> at a given (<code>index</code>) position of the
+     * list.
+     *
+     * @param number The number to be inserted at the end of the list.
+     * @param index  The position at which
+     */
+    public void addAt(int number, int index);
+
+    /**
+     * This method is responsible for extracting the first number of the list.
+     *
+     * @return The number extracted.
+     */
+    public int removeFirst();
+
+    /**
+     * This method is responsible for extracting the last number of the list.
+     *
+     * @return The number extracted.
+     */
+    public int removeLast();
+
+    /**
+     * This method is responsible for storing the <code>number</code> at a given (<code>index</code>) position of the
+     * list.
+     *
+     * @param number The number to be inserted at the end of the list.
+     * @param index  The position at which
+     */
+    public void removeAt(int number, int index);
+}
---
+++ src/datastructures/lists/ListIntArray.java
@@ -0,0 +1,57 @@
+package datastructures.lists;
+
+/**
+ * Created by andres on 5/2/16.
+ */
+public class ListIntArray implements IListInt {
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return 0;
+    }
+
+    @Override
+    public void addAtBeginning(int number) {
+
+    }
+
+    @Override
+    public void addAtEnd(int number) {
+
+    }
+
+    @Override
+    public void addAt(int number, int index) {
+
+    }
+
+    @Override
+    public int removeFirst() {
+        return 0;
+    }
+
+    @Override
+    public int removeLast() {
+        return 0;
+    }
+
+    @Override
+    public void removeAt(int number, int index) {
+
+    }
+}
